b00b4ab58317abe373ef65d7492cb7870af8f219
Harry Cameron <hcameron@google.com>
2015-03-25 17:21:05 +0100

    add powerline placeholder for loginstatus

--- custom/powerline/custom.py
+++ custom/powerline/custom.py
@@ -5,3 +5,7 @@ import subprocess
 def branch(pl):
   status = subprocess.check_output('__git_ps1')
   return status[2:-1] if status else None
+
+def loginstatus(pl):
+  err = subprocess.call(['loginstatus'])
+  return '' if err else None
